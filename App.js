9f385e0fa5b8ec36a2572dcd3ecd3eb1da17bd38
rc-balaji <balajic@drngpit.ac.in>
2023-08-11 01:18:00 +0530

    commit

--- App.js
+++ App.js
@@ -1,11 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { Picker } from '@react-native-picker/picker'; 
+import { useAnimation } from 'react-native-reanimated';
+
+const locations = {
+  TPR: {
+    latitude: 10.7905,
+    longitude: 78.7047,
+  },
+  CBE: {
+    latitude: 11.2588, 
+    longitude: 78.6569,
+  },
+  ERD: {
+    latitude: 11.1271,
+    longitude: 78.0361,
+  },
+  CHE: {
+    latitude: 10.7903,
+    longitude: 78.1392,
+  }
+};
 
 export default function App() {
+
+  const [selectedLocation, setSelectedLocation] = useState('CBE');
+  
+  const carAnimation = useAnimation();
+
+  const animateCar = async (from, to) => {
+    await carAnimation.timing({
+      from: {
+        latitude: from.latitude,
+        longitude: from.longitude,   
+      },
+      to: {
+        latitude: to.latitude,
+        longitude: to.longitude,
+      } 
+    }).start();
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <MapView
+        style={styles.map} 
+        initialRegion={{
+          latitude: 11.1271,
+          longitude: 78.0361,
+          latitudeDelta: 5,
+          longitudeDelta: 5,
+        }}
+      >
+
+        <Marker coordinate={locations.TPR} title="TPR" />
+        <Marker coordinate={locations.CBE} title="CBE" />
+        <Marker coordinate={locations.ERD} title="ERD" />
+        <Marker coordinate={locations.CHE} title="CHE" />
+
+        <Marker  
+          coordinate={carAnimation}
+          anchor={{x: 0.5, y: 0.5}}
+        >
+          <Text style={{fontSize: 25}}>🚗</Text>
+        </Marker>
+
+      </MapView>
+
+      <Picker
+        selectedValue={selectedLocation}
+        onValueChange={(itemValue) => {
+          setSelectedLocation(itemValue);
+          const from = locations[selectedLocation]; 
+          const to = locations[itemValue];
+          animateCar(from, to); 
+        }}
+      >
+        <Picker.Item label="TPR" value="TPR" />
+        <Picker.Item label="CBE" value="CBE" />
+        <Picker.Item label="ERD" value="ERD" />  
+        <Picker.Item label="CHE" value="CHE" />
+      </Picker>
+
     </View>
   );
 }
@@ -13,8 +91,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
---
+++ locations.js
@@ -0,0 +1,8 @@
+// locations.js
+export const locations = {
+    TPR: { x: 50, y: 50 },
+    CBE: { x: 150, y: 50 },
+    ERD: { x: 50, y: 150 },
+    CHE: { x: 150, y: 150 },
+  };
+  
